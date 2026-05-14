8c5ffbb595f39fc779c84ff5c042bfe851e4244e
Dylan Berry <dylanjberry@icloud.com>
2026-05-13 18:41:27 -0700

    finished setting up package

--- src/nldate/_parse.py
+++ src/nldate/_parse.py
@@ -1,5 +1,5 @@
 import datetime as date
 
 
-def parse(date_string, date_starting=date(2026, 5, 13)):
-    return date(2025, 12, 10)
+def parse(date_string, date_starting=date.datetime(2026, 5, 13)):
+    return date.datetime(2025, 10, 12)
